3cf548e30fc0f3eecb1ebd079ed2f601d04ec164
Adam Dinan <adam1989ie@gmail.com>
2023-05-23 12:32:00 +0100

    Return predictions dict for each set of variants provided

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, UploadFile, File
+import logging
 import pandas as pd
 import pickle
 import sklearn
-import logging
 
 app = FastAPI()
 
@@ -14,10 +14,18 @@ except FileNotFoundError:
     best_models = {}
 
 try:
+    with open('metadata.txt', 'rb') as file:
+        meta_data = pd.read_csv(file, sep = '\t')
+except FileNotFoundError:
+    logging.error("Failed to load metadata")
+    meta_data = pd.DataFrame()
+
+try:
     sample_genotypes = pd.read_csv("sample_genotypes.tsv", sep="\t")
 except FileNotFoundError:
     logging.error("Failed to load sample_genotypes.tsv")
     sample_genotypes = pd.DataFrame()
+
 
 class GenotypesData:
     def __init__(self, data: pd.DataFrame):
@@ -34,8 +42,10 @@ class GenotypesData:
         return input_columns == sample_columns
 
     def preprocess_data(self):
-        preprocessed_data = self.data
-        return preprocessed_data
+        geno_list = (self.data)['feature_id'].tolist()
+        genotypes_t = (self.data).iloc[:, 1:].transpose()
+        genotypes_t.columns = geno_list 
+        return genotypes_t
 
 
 @app.post("/predict/")
@@ -45,22 +55,33 @@ def predict_gene_expression(file: UploadFile = File(...)):
     if not genotypes_data.validate_columns():
         return {"message": "Invalid columns"}
 
-    feature_id = genotypes_data.data.columns[0]
+    genotypes_t = genotypes_data.preprocess_data()
 
-    if feature_id not in best_models:
-        return {"message": "Feature not found"}
+    strain_list = list(genotypes_t.index.values)
 
-    model = best_models[feature_id]
+    meta_sub = meta_data.dropna(how='all')
+    meta_sub = meta_sub[meta_sub['sample_name'].isin(strain_list)]
 
-    input_data = genotypes_data.preprocess_data()
+    st_encod = pd.get_dummies(meta_sub['majority_ST'], prefix='ST')
+    st_encod.index = strain_list
 
-    try:
-        predictions = model.predict(input_data)
-    except Exception as e:
-        logging.error(f"Prediction failed: {str(e)}")
-        return {"message": "Prediction failed"}
+    vars_st = genotypes_t.merge(st_encod, left_index=True, right_index=True)
 
-    return {"predictions": predictions.tolist()}
+    X = vars_st.values
+    scaler = sklearn.preprocessing.StandardScaler()
+    X = scaler.fit_transform(X) 
+
+    predictions_dict = {}
+
+    for feature_id, model in best_models.items():
+        try:
+            predictions = model.predict(X)
+            predictions_dict[feature_id] = predictions.tolist()
+        except Exception as e:
+            logging.error(f"Prediction failed: {str(e)} for {feature_id}")
+            return {"message": "Prediction failed"}
+        
+    return predictions_dict
 
 
 logging.basicConfig(level=logging.ERROR, filename="api.log", filemode="a",
