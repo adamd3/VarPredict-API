c08be7b0706c2bf2e147ed6ac6c081e8d679ddcf
Adam Dinan <adam1989ie@gmail.com>
2023-05-27 18:38:09 +0100

    Run tests on AWS after pushing container

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,7 @@ def file_path():
     return "./model_data/sample_genotypes.tsv"
 
 def test_predict_gene_expression(file_path):
-    url = "http://localhost:80/predict/"  
+    url = "13.53.42.163:80/predict/"  
     files = {"file": open(file_path, "rb")}
     response = requests.post(url, files=files)
     response_json = response.json()
