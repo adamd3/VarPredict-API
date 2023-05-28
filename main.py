08ba334946292db85f58e147f1b7bb4a4f832081
Adam Dinan <adam1989ie@gmail.com>
2023-05-28 16:46:52 +0100

    Load model in Docker container

--- main.py
+++ main.py
@@ -1,32 +1,9 @@
 from fastapi import FastAPI, UploadFile, File
 import logging
 import pandas as pd
-import pickle
 import sklearn
 
 app = FastAPI()
-
-try:
-    with open('./model_data/best_models.pkl', 'rb') as file:
-        best_models = pickle.load(file)
-except FileNotFoundError:
-    logging.error("Failed to load best_models.pkl")
-    best_models = {}
-
-try:
-    with open('./model_data/metadata.txt', 'rb') as file:
-        meta_data = pd.read_csv(file, sep = '\t')
-except FileNotFoundError:
-    logging.error("Failed to load metadata")
-    meta_data = pd.DataFrame()
-
-try:
-    sample_genotypes = pd.read_csv(
-        './model_data/sample_genotypes.tsv', sep='\t')
-except FileNotFoundError:
-    logging.error("Failed to load sample_genotypes.tsv")
-    sample_genotypes = pd.DataFrame()
-
 
 class GenotypesData:
     def __init__(self, data: pd.DataFrame):
@@ -45,7 +22,7 @@ class GenotypesData:
     def preprocess_data(self):
         geno_list = (self.data)['feature_id'].tolist()
         genotypes_t = (self.data).iloc[:, 1:].transpose()
-        genotypes_t.columns = geno_list 
+        genotypes_t.columns = geno_list list
         return genotypes_t
 
 
